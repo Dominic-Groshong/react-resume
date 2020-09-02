0a55a9670c69bd57e0f4e734703c1eaa7effb7a3
Dominic Groshong <groshond@gmail.com>
2020-09-02 07:14:35 -0700

    updated metadata, changed favicon, renamed CNAME

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import logo from './logo.svg';
 import 'styles/css/styles.css';
 
+
 import Layout from 'containers/Layout';
 
 function App() {
--- frontend/src/containers/Footer.js
+++ frontend/src/containers/Footer.js
@@ -5,7 +5,7 @@ class Footer extends React.Component {
         return (
             <footer>
                 <section className="container">
-                    <p>©2020 Dominic Groshong |  All rights reserved  | For more information email me at <a href="mailto:&#103;&#114;&#111;&#115;&#104;&#111;&#110;&#100;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;">&#103;&#114;&#111;&#115;&#104;&#111;&#110;&#100;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;</a> </p>
+                    <p>©{`${new Date().getFullYear()}`} Dominic Groshong |  All rights reserved  | For more information email me at <a href="mailto:&#103;&#114;&#111;&#115;&#104;&#111;&#110;&#100;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;">&#103;&#114;&#111;&#115;&#104;&#111;&#110;&#100;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;</a></p>
                     <p>This site is on GitHub Pages via <a href="https://reactjs.org/" rel="noopener noreferrer" target="_blank">React.js</a>, and being served content via a REST API on a <a href="https://www.djangoproject.com/" rel="noopener noreferrer" target="_blank">Django</a> framework hosted by <a href="http://pythonanywhere.com/" rel="noopener noreferrer" target="_blank">Python Anywhere</a>.</p>
                 </section>
             </footer>
